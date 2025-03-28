84515b2035b1dbe65c283037c8187bc7cc00bf4d
grohith327 <grohith327@gmail.com>
2025-03-27 18:34:02 -0700

    Add request type as a dropdown

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,50 @@ use crossterm::{
 use ratatui::{prelude::*, widgets::*};
 use std::{io, time::Duration};
 
+struct Dropdown {
+    items: Vec<String>,
+    state: ListState,
+    open: bool,
+}
+
+impl Dropdown {
+    fn new(items: Vec<String>) -> Self {
+        let mut state = ListState::default();
+        state.select(Some(0));
+        Self {
+            items,
+            state,
+            open: false,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => (i + 1) % self.items.len(),
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 && self.items.len() > 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn toggle(&mut self) {
+        self.open = !self.open
+    }
+}
+
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -23,6 +67,15 @@ fn run_app(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<
     let mut request_body = String::from("{key: value}");
     let mut response = String::from("{key: value}");
 
+    let mut dropdown = Dropdown::new(vec![
+        "POST".to_string(),
+        "GET".to_string(),
+        "PUT".to_string(),
+        "UPDATE".to_string(),
+        "DELETE".to_string(),
+    ]);
+    let mut dropdown_val = String::from("");
+
     loop {
         terminal.draw(|frame| {
             let chunks = Layout::default()
@@ -30,8 +83,9 @@ fn run_app(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<
                 .constraints(
                     [
                         Constraint::Percentage(10),
-                        Constraint::Percentage(45),
-                        Constraint::Percentage(45),
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(40),
+                        Constraint::Percentage(40),
                     ]
                     .as_ref(),
                 )
@@ -49,6 +103,16 @@ fn run_app(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<
                         )
                         .border_style(Style::default().fg(Color::LightBlue)),
                 );
+
+            let request_type_block = Block::default()
+                .borders(Borders::ALL)
+                .title("Request Type")
+                .title_style(
+                    Style::default()
+                        .fg(Color::LightYellow)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .border_style(Style::default().fg(Color::LightBlue));
 
             let request_body_block = Paragraph::new(Span::styled(
                 &request_body,
@@ -78,15 +142,67 @@ fn run_app(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> io::Result<
                         .border_style(Style::default().fg(Color::LightBlue)),
                 );
 
+            if dropdown.open {
+                let items: Vec<ListItem> = dropdown
+                    .items
+                    .iter()
+                    .map(|item| ListItem::new(Span::raw(item.clone())))
+                    .collect();
+
+                let list = List::new(items)
+                    .block(Block::default().borders(Borders::ALL).title("Request Type"))
+                    .highlight_style(Style::default().fg(Color::Yellow));
+
+                frame.render_stateful_widget(list, chunks[1], &mut dropdown.state);
+            } else {
+                let selected_request_type = Paragraph::new(Span::styled(
+                    &dropdown_val,
+                    Style::default().fg(Color::White),
+                ))
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .title("Request Type")
+                        .title_style(
+                            Style::default()
+                                .fg(Color::LightYellow)
+                                .add_modifier(Modifier::BOLD),
+                        )
+                        .border_style(Style::default().fg(Color::LightBlue)),
+                );
+
+                frame.render_widget(selected_request_type, chunks[1]);
+            }
+
             frame.render_widget(url_block, chunks[0]);
-            frame.render_widget(request_body_block, chunks[1]);
-            frame.render_widget(response_block, chunks[2]);
+            frame.render_widget(request_type_block, chunks[1]);
+            frame.render_widget(request_body_block, chunks[2]);
+            frame.render_widget(response_block, chunks[3]);
         })?;
 
         if event::poll(Duration::from_millis(100))? {
             if let event::Event::Key(key) = event::read()? {
-                if key.code == KeyCode::Char('q') {
-                    break;
+                match key.code {
+                    KeyCode::Char(' ') => dropdown.toggle(),
+                    KeyCode::Down => dropdown.next(),
+                    KeyCode::Up => dropdown.previous(),
+                    KeyCode::Enter => {
+                        if dropdown.open {
+                            if let Some(i) = dropdown.state.selected() {
+                                dropdown_val = dropdown.items[i].clone();
+                            }
+                            dropdown.toggle();
+                        }
+                    }
+                    KeyCode::Char('q') => break,
+                    KeyCode::Esc => {
+                        if dropdown.open {
+                            dropdown.toggle();
+                        } else {
+                            break;
+                        }
+                    }
+                    _ => {}
                 }
             }
         }
